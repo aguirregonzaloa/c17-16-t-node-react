24a8eef41a8699682baf7bf9dac057794a32a9e5
Gonzalo Aguirre <gonza1810@gmail.com>
2024-04-03 22:19:49 -0300

    Agregando componentes de Login y Register

--- apps/frontend/src/App.jsx
+++ apps/frontend/src/App.jsx
@@ -1,38 +1,14 @@
-import { useState } from 'react';
-import { Card, Text, CardBody, Button, Container } from '@chakra-ui/react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
-import './App.css';
+import { Container, Flex } from '@chakra-ui/react';
+import Login from './components/auth/Login';
+import Register from './components/auth/Register';
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <Container height={'100vh'}>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-
-      <Card>
-        <CardBody>
-          <Button
-            colorScheme="blue"
-            onClick={() => setCount((count) => count + 1)}
-          >
-            count is {count}
-          </Button>
-          <Text>
-            Edit <code>src/App.jsx</code> and save to test HMR
-          </Text>
-          <Text> Click on the Vite and React logos to learn more</Text>
-        </CardBody>
-      </Card>
+    <Container height={'100vh'} maxWidth={'100vw'}>
+      <Flex justify={'center'} align={'center'} gap={'100px'}>
+        <Login />
+        <Register />
+      </Flex>
     </Container>
   );
 }
