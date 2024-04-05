c8f63807b8b0e5782592f07cf04b90d60ca60481
FlorenciaNieva <florencianieva930@gmail.com>
2024-04-05 20:18:24 -0300

    Agrego links de navegación

--- apps/frontend/src/components/NavBar/NavBar.jsx
+++ apps/frontend/src/components/NavBar/NavBar.jsx
@@ -1,7 +1,22 @@
-import React from 'react'
+import React from "react";
+import { Link as ChakraLink } from "@chakra-ui/react";
+import { Link as ReactRouterLink } from "react-router-dom";
 
 export default function NavBar() {
   return (
-    <div>NavBar</div>
-  )
+    <nav>
+      <ChakraLink as={ReactRouterLink} to="/" m="2">
+        Inicio
+      </ChakraLink>
+      <ChakraLink as={ReactRouterLink} to="/about-we" m="2">
+        Nosotros
+      </ChakraLink>
+      <ChakraLink as={ReactRouterLink} to="/rates" m="2">
+        Tarifas
+      </ChakraLink>
+      <ChakraLink as={ReactRouterLink} to="/login" m="2">
+        Ingresar
+      </ChakraLink>
+    </nav>
+  );
 }
