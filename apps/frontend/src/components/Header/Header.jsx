9b25a2690b555a0fba0c29a0aadef1295fabd741
FlorenciaNieva <florencianieva930@gmail.com>
2024-04-05 20:17:34 -0300

    Creo componente NavBar con estructura inicial

--- apps/frontend/src/components/Header/Header.jsx
+++ apps/frontend/src/components/Header/Header.jsx
@@ -1,7 +1,24 @@
-import React from 'react'
+import React from "react";
+import { Link as ChakraLink } from "@chakra-ui/react";
+import { Link as ReactRouterLink } from "react-router-dom";
 
 export default function Header() {
   return (
-    <div>Header</div>
-  )
+    <header>
+      <nav>
+        <ChakraLink as={ReactRouterLink} to="/" m="2">
+          Inicio
+        </ChakraLink>
+        <ChakraLink as={ReactRouterLink} to="/about-we" m="2">
+          Nosotros
+        </ChakraLink>
+        <ChakraLink as={ReactRouterLink} to="/rates" m="2">
+          Tarifas
+        </ChakraLink>
+        <ChakraLink as={ReactRouterLink} to="/login" m="2">
+          Ingresar
+        </ChakraLink>
+      </nav>
+    </header>
+  );
 }
