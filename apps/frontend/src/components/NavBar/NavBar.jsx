09b00a4f652c33dee205191c894762a1571474f1
FlorenciaNieva <florencianieva930@gmail.com>
2024-04-07 18:56:11 -0300

    Agrego prop direction en el componente para modificar según el tamaño de pantalla

--- apps/frontend/src/components/NavBar/NavBar.jsx
+++ apps/frontend/src/components/NavBar/NavBar.jsx
@@ -1,55 +1,60 @@
 import React from "react";
-import { Link as ChakraLink, Button } from "@chakra-ui/react";
+import { Link as ChakraLink, Button, Flex } from "@chakra-ui/react";
 import { Link as ReactRouterLink } from "react-router-dom";
 
-export default function NavBar() {
+export default function NavBar({ direction }) {
   return (
     <nav>
-      <ChakraLink
-        as={ReactRouterLink}
-        to="/"
-        m="3"
-        color="gris.400"
-        _hover={{ color: "azulacento.500", fontFamily: "Poppins-SemiBold" }}
-        _focus={{ color: "azulacento.500", fontFamily: "Poppins-SemiBold" }}
-        _active={{ color: "azulacento.500", fontFamily: "Poppins-SemiBold" }}
-      >
-        Inicio
-      </ChakraLink>
-      <ChakraLink
-        as={ReactRouterLink}
-        to="/about-we"
-        m="3"
-        color="gris.400"
-        _hover={{ color: "azulacento.500", fontFamily: "Poppins-SemiBold" }}
-        _focus={{ color: "azulacento.500", fontFamily: "Poppins-SemiBold" }}
-        _active={{ color: "azulacento.500", fontFamily: "Poppins-SemiBold" }}
-      >
-        Nosotros
-      </ChakraLink>
-      <ChakraLink
-        as={ReactRouterLink}
-        to="/rates"
-        m="3"
-        color="gris.400"
-        _hover={{ color: "azulacento.500", fontFamily: "Poppins-SemiBold" }}
-        _focus={{ color: "azulacento.500", fontFamily: "Poppins-SemiBold" }}
-        _active={{ color: "azulacento.500", fontFamily: "Poppins-SemiBold" }}
-      >
-        Tarifas
-      </ChakraLink>
-      <Button
-        as={ReactRouterLink}
-        to="/login"
-        m="3"
-        bg="verdeacento.600"
-        color="gris.800"
-        _hover={{ bg: "verdeacento.500" }}
-        _focus={{ bg: "verdeacento.500" }}
-        _active={{ bg: "verdeacento.500" }}
-      >
-        Ingresar
-      </Button>
+      <Flex m={2} direction={direction}>
+        <ChakraLink
+          as={ReactRouterLink}
+          to="/"
+          m="3"
+          alignSelf="center"
+          color="gris.400"
+          _hover={{ color: "azulacento.500", fontFamily: "Poppins-SemiBold" }}
+          _focus={{ color: "azulacento.500", fontFamily: "Poppins-SemiBold" }}
+          _active={{ color: "azulacento.500", fontFamily: "Poppins-SemiBold" }}
+        >
+          Inicio
+        </ChakraLink>
+        <ChakraLink
+          as={ReactRouterLink}
+          to="/about-we"
+          m="3"
+          alignSelf="center"
+          color="gris.400"
+          _hover={{ color: "azulacento.500", fontFamily: "Poppins-SemiBold" }}
+          _focus={{ color: "azulacento.500", fontFamily: "Poppins-SemiBold" }}
+          _active={{ color: "azulacento.500", fontFamily: "Poppins-SemiBold" }}
+        >
+          Nosotros
+        </ChakraLink>
+        <ChakraLink
+          as={ReactRouterLink}
+          to="/rates"
+          m="3"
+          alignSelf="center"
+          color="gris.400"
+          _hover={{ color: "azulacento.500", fontFamily: "Poppins-SemiBold" }}
+          _focus={{ color: "azulacento.500", fontFamily: "Poppins-SemiBold" }}
+          _active={{ color: "azulacento.500", fontFamily: "Poppins-SemiBold" }}
+        >
+          Tarifas
+        </ChakraLink>
+        <Button
+          as={ReactRouterLink}
+          to="/login"
+          m="3"
+          bg="verdeacento.600"
+          color="gris.800"
+          _hover={{ bg: "verdeacento.500" }}
+          _focus={{ bg: "verdeacento.500" }}
+          _active={{ bg: "verdeacento.500" }}
+        >
+          Ingresar
+        </Button>
+      </Flex>
     </nav>
   );
 }
