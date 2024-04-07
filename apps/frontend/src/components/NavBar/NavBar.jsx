53ffc3646375fb24d63c5e4ca289955f921459d0
FlorenciaNieva <florencianieva930@gmail.com>
2024-04-06 21:17:33 -0300

    Agrego estilos a los links de navegación

--- apps/frontend/src/components/NavBar/NavBar.jsx
+++ apps/frontend/src/components/NavBar/NavBar.jsx
@@ -1,22 +1,51 @@
 import React from "react";
-import { Link as ChakraLink } from "@chakra-ui/react";
+import { Link as ChakraLink, Button } from "@chakra-ui/react";
 import { Link as ReactRouterLink } from "react-router-dom";
 
 export default function NavBar() {
   return (
     <nav>
-      <ChakraLink as={ReactRouterLink} to="/" m="2">
+      <ChakraLink
+        as={ReactRouterLink}
+        to="/"
+        m="3"
+        color="gris.400"
+        _hover={{ color: "azulacento.500", fontFamily: "Poppins-SemiBold" }}
+        _focus={{ color: "azulacento.500", fontFamily: "Poppins-SemiBold" }}
+      >
         Inicio
       </ChakraLink>
-      <ChakraLink as={ReactRouterLink} to="/about-we" m="2">
+      <ChakraLink
+        as={ReactRouterLink}
+        to="/about-we"
+        m="3"
+        color="gris.400"
+        _hover={{ color: "azulacento.500", fontFamily: "Poppins-SemiBold" }}
+        _focus={{ color: "azulacento.500", fontFamily: "Poppins-SemiBold" }}
+      >
         Nosotros
       </ChakraLink>
-      <ChakraLink as={ReactRouterLink} to="/rates" m="2">
+      <ChakraLink
+        as={ReactRouterLink}
+        to="/rates"
+        m="3"
+        color="gris.400"
+        _hover={{ color: "azulacento.500", fontFamily: "Poppins-SemiBold" }}
+        _focus={{ color: "azulacento.500", fontFamily: "Poppins-SemiBold" }}
+      >
         Tarifas
       </ChakraLink>
-      <ChakraLink as={ReactRouterLink} to="/login" m="2">
+      <Button
+        as={ReactRouterLink}
+        to="/login"
+        m="3"
+        bg="verdeacento.600"
+        color="gris.800"
+        _hover={{ bg:"verdeacento.500" }}
+        _focus={{ bg:"verdeacento.500" }}
+      >
         Ingresar
-      </ChakraLink>
+      </Button>
     </nav>
   );
 }
