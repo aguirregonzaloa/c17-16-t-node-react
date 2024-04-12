4d69823484547325a52ee07830ea8138813cc546
FlorenciaNieva <florencianieva930@gmail.com>
2024-04-11 22:52:46 -0300

    Agrego codigo reutilizable para el componente InfoSection

--- apps/frontend/src/components/ItemInfoSection/ItemInfoSection.jsx
+++ apps/frontend/src/components/ItemInfoSection/ItemInfoSection.jsx
@@ -1,7 +1,29 @@
-import React from 'react'
+import React from "react";
+import { Image, Text } from "@chakra-ui/react";
 
-export default function ItemInfoSection() {
+export default function ItemInfoSection({ icon, title, text }) {
   return (
-    <div>ItemInfoSection</div>
-  )
+    <>
+      <Image src={icon} alt="icon" my={2} />
+      <Text
+        fontFamily="Poppins-SemiBold"
+        fontWeight="600"
+        fontSize="18px"
+        lineHeight="27px"
+        color="azulacento.500"
+        mb={2}
+      >
+        {title}
+      </Text>
+      <Text
+        fontFamily="Poppins-Regular"
+        fontWeight="400"
+        fontSize="16px"
+        lineHeight="24px"
+        color="azulacento.500"
+      >
+        {text}
+      </Text>
+    </>
+  );
 }
