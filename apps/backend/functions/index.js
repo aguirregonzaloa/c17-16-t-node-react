81c64680a5883eccbc7b757e452acdba7705ae9e
Jesond <js.pineda.sa@gmail.com>
2024-04-12 14:40:53 -0600

    [feature] se agrego la validacion de token para los endpoins de app, los endpoints de appPublic no necesitan verificacion de token

--- apps/backend/functions/index.js
+++ apps/backend/functions/index.js
@@ -47,59 +47,64 @@ const firestore = getFirestore();
 app.use(cors({ origin: true }));
 appPublic.use(cors({ origin: true }));
 
-// const validateFirebaseIdToken = async (req, res, next) => {
-//     console.log("Check if request is authorized with Firebase ID token");
+const validateFirebaseIdToken = async (req, res, next) => {
+    console.log("Check if request is authorized with Firebase ID token");
   
-//     if (
-//       (!req.headers.authorization ||
-//         !req.headers.authorization.startsWith("Bearer ")) &&
-//       !(req.cookies && req.cookies.__session)
-//     ) {
-//       console.error(
-//         "No Firebase ID token was passed as a Bearer token in the Authorization header.",
-//         "Make sure you authorize your request by providing the following HTTP header:",
-//         "Authorization: Bearer <Firebase ID Token>",
-//         'or by passing a "__session" cookie.'
-//       );
-//       res.status(403).send("Unauthorized");
-//       return;
-//     }
+    if (
+      (!req.headers.authorization ||
+        !req.headers.authorization.startsWith("Bearer ")) &&
+      !(req.cookies && req.cookies.__session)
+    ) {
+      console.error(
+        "No Firebase ID token was passed as a Bearer token in the Authorization header.",
+        "Make sure you authorize your request by providing the following HTTP header:",
+        "Authorization: Bearer <Firebase ID Token>",
+        'or by passing a "__session" cookie.'
+      );
+      res.status(403).send("Unauthorized");
+      return;
+    }
   
-//     let idToken;
-//     if (
-//       req.headers.authorization &&
-//       req.headers.authorization.startsWith("Bearer ")
-//     ) {
-//       console.log('Found "Authorization" header');
-//       // Read the ID Token from the Authorization header.
-//       idToken = req.headers.authorization.split("Bearer ")[1];
-//     } else if (req.cookies) {
-//       console.log('Found "__session" cookie');
-//       // Read the ID Token from cookie.
-//       idToken = req.cookies.__session;
-//     } else {
-//       // No cookie
-//       res.status(403).send("Unauthorized");
-//       return;
-//     }
+    let idToken;
+    if (
+      req.headers.authorization &&
+      req.headers.authorization.startsWith("Bearer ")
+    ) {
+      console.log('Found "Authorization" header');
+      // Read the ID Token from the Authorization header.
+      idToken = req.headers.authorization.split("Bearer ")[1];
+    } else if (req.cookies) {
+      console.log('Found "__session" cookie');
+      // Read the ID Token from cookie.
+      idToken = req.cookies.__session;
+    } else {
+      // No cookie
+      res.status(403).send("Unauthorized");
+      return;
+    }
   
-//     try {
-//       const decodedIdToken = await admin.auth().verifyIdToken(idToken);
-//       console.log("ID Token correctly decoded", decodedIdToken);
-//       req.user = decodedIdToken;
-//       next();
-//       return;
-//     } catch (error) {
-//       console.error("Error while verifying Firebase ID token:", error);
-//       res.status(403).send("Unauthorized");
-//       return;
-//     }
-//   };
-// app.use(validateFirebaseIdToken);
+    try {
+      const decodedIdToken = await admin.auth().verifyIdToken(idToken);
+      console.log("ID Token correctly decoded", decodedIdToken);
+      req.user = decodedIdToken;
+      next();
+      return;
+    } catch (error) {
+      console.error("Error while verifying Firebase ID token:", error);
+      res.status(403).send("Unauthorized");
+      return;
+    }
+  };
+app.use(validateFirebaseIdToken);
 
-// Registro
+//#region Endpoins Privados
 
-app.post("/registro", async (req, res) => {
+exports.app = functions.https.onRequest(app);
+
+//#region Endpoins Publicos
+
+ // Registro de Usuario
+appPublic.post("/registro", async (req, res) => {
   const { nombre, correo, contraseña } = req.body;
 
   // Validar la entrada
@@ -110,11 +115,6 @@ app.post("/registro", async (req, res) => {
   // Crear el usuario en Firebase
   try {
     
-    // const signInMethods = await auth.fetchSignInMethodsForEmail(correo);
-    // if (signInMethods.length > 0) {
-    //   throw new Error("El correo electrónico ya está registrado");
-    // }
-
     const userCredential = await createUserWithEmailAndPassword(auth, correo, contraseña);
     const docRef = await firestore
       .collection("users")
@@ -123,36 +123,36 @@ app.post("/registro", async (req, res) => {
         nombre: nombre,
         correo: correo,
       });
-      console.log(docRef)
+
     // Enviar respuesta de éxito
-    res.status(201).send({ message: "Usuario registrado correctamente", status: true });
+    res.status(201).send({ message: "Usuario registrado correctamente", status: true, token: userCredential.user.accessToken });
 
   } catch (error) {
     // Manejar errores de Firebase
     console.error("Error al registrar usuario:", error);
-    res.status(400).send(error.message);
+    res.status(400).send({ message: "Error al registrar usuario", status: false });
   }
 });
 
-// Login
-app.post("/login", async (req, res) => {
+// Login de Usuario
+appPublic.post("/login", async (req, res) => {
   const { correo, contraseña } = req.body;
 
   // Validate input
   if (!correo || !contraseña) {
-    return res.status(400).send("Faltan datos obligatorios (correo y contraseña)");
+    return res.status(400).send({ message: "Faltan datos obligatorios (correo y contraseña)", status: false });
   }
 
   try {
     // Sign in user with email and password
     const userCredential = await signInWithEmailAndPassword(auth, correo, contraseña);
-    
+  
     // You can send additional information like user ID or a custom token in the response
-    res.status(200).send({ message: "Usuario autenticado correctamente", status: true });
+    res.status(200).send({ message: "Usuario autenticado correctamente", status: true, token: userCredential.user.accessToken });
   } catch (error) {
     console.error("Error al iniciar sesión:", error);
-    res.status(401).send("Correo o contraseña incorrectos"); // Adjust error message as needed
+    res.status(401).send({ message: "Correo o contraseña incorrecta", status: false}); // Adjust error message as needed
   }
 });
 
-exports.app = functions.https.onRequest(app);
+exports.appPublic = functions.https.onRequest(appPublic);
