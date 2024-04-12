452fb5d4a0858dc2716f99f00663c791c3b8bf7d
FlorenciaNieva <florencianieva930@gmail.com>
2024-04-11 22:54:10 -0300

    Agrego InfoSection con sus repectivas props al Home

--- apps/frontend/src/components/Home/Home.jsx
+++ apps/frontend/src/components/Home/Home.jsx
@@ -1,10 +1,27 @@
-import React from 'react'
-import Banner from '../Banner/Banner'
+import React from "react";
+import Banner from "../Banner/Banner";
+import InfoSection from "../InfoSection/InfoSection";
+import ImgDog from "../../assets/img/Dog1.png";
+import ImgCat from "../../assets/img/Cat1.png";
 
 export default function Home() {
   return (
     <>
       <Banner />
+      <InfoSection
+        iconAnimal={ImgDog}
+        text="Somos el intermediario que se encarga de reunirte con el mejor cuidado para tu mascota."
+        title="Nuestros Servicios"
+        order="1"
+        textButton="Más de nosotros"
+      />
+      <InfoSection
+        iconAnimal={ImgCat}
+        text="Todos nuestros paseadores tienen un certificado de cuidador."
+        title="Certificado de cuidador"
+        order="2"
+        textButton="Más sobre esto"
+      />
     </>
-  )
+  );
 }
