633988ed3de451df56eef8c9040895bd3d17faac
FlorenciaNieva <florencianieva930@gmail.com>
2024-04-13 17:13:43 -0300

    Agrego código reutilizable para los pasos de las funcionalidad

--- apps/frontend/src/components/ProcessFunctioning/ProcessFunctioning.jsx
+++ apps/frontend/src/components/ProcessFunctioning/ProcessFunctioning.jsx
@@ -1,7 +1,23 @@
-import React from 'react'
+import { Box, Image, Heading, Text } from "@chakra-ui/react";
+import React from "react";
 
-export default function ProcessFunctioning() {
+export default function ProcessFunctioning({ image, title, description }) {
   return (
-    <div>ProcessFunctioning</div>
-  )
+    <Box w="259px">
+      <Image src={image} alt="Imagen de perrito" />
+      <Box gap="5px">
+        <Heading
+          color="verdeacento.800"
+          size="16px"
+          fontWeight="600"
+          lineHeight="24px"
+        >
+          {title}
+        </Heading>
+        <Text color="gris.600" size="14px" fontWeight="400" lineHeight="21px">
+          {description}
+        </Text>
+      </Box>
+    </Box>
+  );
 }
