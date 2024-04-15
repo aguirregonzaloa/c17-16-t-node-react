9ebfb5f4047d48ec0173e23fb8f20fb6a3b4fe39
FlorenciaNieva <florencianieva930@gmail.com>
2024-04-15 17:44:37 -0300

    Elimino archivos que no se utilizaran

--- apps/frontend/src/components/AboutWe/AboutWe.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function AboutWe() {
-  return (
-    <div>AboutWe</div>
-  )
-}
--- apps/frontend/src/components/Rates/Rates.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function Rates() {
-  return (
-    <div>Rates</div>
-  )
-}
