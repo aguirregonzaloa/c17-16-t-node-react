b9438ee9fcb4a5690885a7aa6ff465147d014d4e
FlorenciaNieva <florencianieva930@gmail.com>
2024-04-16 09:13:40 -0300

    Modifico el nombre del componente de BestCaregivers a TopCaregivers

--- apps/frontend/src/components/BestCaregivers/BestCaregivers.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function BestCaregivers() {
-  return (
-    <div>BestCaregivers</div>
-  )
-}
---
+++ apps/frontend/src/components/TopCaregivers/TopCaregivers.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function TopCaregivers() {
+  return (
+    <div>TopCaregivers</div>
+  )
+}
