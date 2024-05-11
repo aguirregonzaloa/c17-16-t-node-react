bd713f3d041ae8e639441f596346ee531914cdb1
FlorenciaNieva <florencianieva930@gmail.com>
2024-05-11 20:20:12 -0300

    Modifico la lógica de mostrar el spinner o al cuidador y agrego componente TabsDetailsCaregiver

--- apps/frontend/src/components/DetailsCaregiver/DetailsCaregiver.jsx
+++ apps/frontend/src/components/DetailsCaregiver/DetailsCaregiver.jsx
@@ -3,6 +3,7 @@ import { useParams } from "react-router-dom";
 import { getCareGivers } from "../../utils/api/CareGivers/CareGivers";
 import { Box, Flex, Heading, Image, Spinner, Text } from "@chakra-ui/react";
 import IconLocation from "../../assets/icons/IconLocation.svg";
+import TabsDetailsCaregiver from "../TabsDetailsCaregiver/TabsDetailsCaregiver";
 
 export default function DetailsCaregiver() {
   const { idCaregiver } = useParams();
@@ -28,59 +29,65 @@ export default function DetailsCaregiver() {
     fetchCareGiver();
   }, [idCaregiver]);
 
+  if (!caregiver) {
+    return (
+      <Flex justify="center" pt={20} minH="100vh">
+        <Spinner size="lg" color="azulacento.600" />
+      </Flex>
+    );
+  }
+
   return (
-    <Flex justify="center" pt={20} minH="100vh">
-      {caregiver ? (
-        <Flex gap={6}>
-          <Image src={caregiver.photo} boxSize="180px" borderRadius="8px" />
-          <Flex direction="column" gap={2}>
+    <Flex
+      direction="column"
+      pt={20}
+      minH="100vh"
+      mx={{ base: "20px", md: "100px" }}
+    >
+      <Flex gap={6} py={10}>
+        <Image src={caregiver.photo} boxSize="180px" borderRadius="8px" />
+        <Flex direction="column" gap={2}>
+          <Text
+            fontFamily="Poppins-Regular"
+            fontStyle="italic"
+            color="verdeacento.800"
+            fontSize="14px"
+          >
+            Guarderia
+          </Text>
+          <Heading fontFamily="Poppins-Medium" fontSize="26px" color="gris.800">
+            {caregiver.name}
+          </Heading>
+          <Box>
             <Text
-              fontFamily="Poppins-Regular"
-              fontStyle="italic"
-              color="verdeacento.800"
+              fontWeight="400"
               fontSize="14px"
+              lineHeight="24px"
+              bg="azulacento.100"
+              color="azulacento.500"
+              borderRadius="8px"
+              textAlign="center"
+              mt={1}
+              p="2px 8px"
+              display="inline-block"
             >
-              Guarderia
+              Vacante
             </Text>
-            <Heading
-              fontFamily="Poppins-Medium"
-              fontSize="26px"
-              color="gris.800"
+          </Box>
+          <Flex gap={2}>
+            <Image src={IconLocation} alt="Icono de ubicación" />
+            <Text
+              fontSize="14px"
+              fontWeight="400"
+              lineHeight="24px"
+              color="gris.600"
             >
-              {caregiver.name}
-            </Heading>
-            <Box>
-              <Text
-                fontWeight="400"
-                fontSize="14px"
-                lineHeight="24px"
-                bg="azulacento.100"
-                color="azulacento.500"
-                borderRadius="8px"
-                textAlign="center"
-                mt={1}
-                p="2px 8px"
-                display="inline-block"
-              >
-                Vacante
-              </Text>
-            </Box>
-            <Flex gap={2}>
-              <Image src={IconLocation} alt="Icono de ubicación" />
-              <Text
-                fontSize="14px"
-                fontWeight="400"
-                lineHeight="24px"
-                color="gris.600"
-              >
-                Palermo, Buenos Aires
-              </Text>
-            </Flex>
+              Palermo, Buenos Aires
+            </Text>
           </Flex>
         </Flex>
-      ) : (
-        <Spinner size="lg" color="azulacento.600" />
-      )}
+      </Flex>
+      <TabsDetailsCaregiver />
     </Flex>
   );
 }
