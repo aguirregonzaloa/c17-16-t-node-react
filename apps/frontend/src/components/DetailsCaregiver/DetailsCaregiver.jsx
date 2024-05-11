819e6c9022818f4eaa45f7ec4cd43f35751d9035
FlorenciaNieva <florencianieva930@gmail.com>
2024-05-11 19:37:30 -0300

    Agrego función para traer la información del cuidador según el id de la url

--- apps/frontend/src/components/DetailsCaregiver/DetailsCaregiver.jsx
+++ apps/frontend/src/components/DetailsCaregiver/DetailsCaregiver.jsx
@@ -1,7 +1,41 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { getCareGivers } from "../../utils/api/CareGivers/CareGivers";
+import { Box, Flex, Spinner } from "@chakra-ui/react";
 
 export default function DetailsCaregiver() {
+  const { idCaregiver } = useParams();
+  const [caregiver, setCaregiver] = useState(null);
+
+  useEffect(() => {
+    const fetchCareGiver = async () => {
+      try {
+        const caregiversData = await getCareGivers();
+        const caregiverFound = caregiversData.data.find(
+          (item) => item.idCuidador === idCaregiver
+        );
+        if (caregiverFound) {
+          setCaregiver(caregiverFound);
+          // console.log(caregiverFound)
+        } else {
+          console.log("Cuidador no encontrado");
+        }
+      } catch (error) {
+        console.error("Error al obtener los cuidadores:", error);
+      }
+    };
+    fetchCareGiver();
+  }, [idCaregiver]);
+
   return (
-    <div>DetailsCaregiver</div>
-  )
+    <Flex justify="center" pt={20} minH="100vh">
+      {caregiver ? (
+        <Box>
+          <h2>{caregiver.name}</h2>
+        </Box>
+      ) : (
+        <Spinner size="lg" color="azulacento.600" />
+      )}
+    </Flex>
+  );
 }
