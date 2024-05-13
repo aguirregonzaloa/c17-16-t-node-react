fcae3e5b88f6fb4e8ab804b72c34afb5be76b180
FlorenciaNieva <florencianieva930@gmail.com>
2024-05-13 16:31:14 -0300

    Agrego componente AccordionDetailsCaregiver para el diseño responsive de los tabs

---
+++ apps/frontend/src/components/AccordionDetailsCaregiver/AccordionDetailsCaregiver.jsx
@@ -0,0 +1,98 @@
+import React from "react";
+import {
+  Accordion,
+  AccordionItem,
+  AccordionButton,
+  AccordionPanel,
+  AccordionIcon,
+  Box,
+} from "@chakra-ui/react";
+import TabAboutMe from "../TabAboutMe/TabAboutMe";
+import TabServicesAndRates from "../TabServicesAndRates/TabServicesAndRates";
+import TabReview from "../TabReview/TabReview";
+import TabLocation from "../TabLocation/TabLocation";
+import TabAvailability from "../TabAvailability/TabAvailability";
+
+export default function AccordionDetailsCaregiver({
+  name,
+  aboutMe,
+  catAccepted,
+  dogAccepted,
+}) {
+  return (
+    <Accordion allowToggle>
+      <AccordionItem>
+        <h2>
+          <AccordionButton>
+            <Box as="span" flex="1" textAlign="left" fontFamily="Poppins-Medium" fontSize="15px">
+              Sobre mí
+            </Box>
+            <AccordionIcon />
+          </AccordionButton>
+        </h2>
+        <AccordionPanel pb={4}>
+          <TabAboutMe name={name} aboutMe={aboutMe} />
+        </AccordionPanel>
+      </AccordionItem>
+
+      <AccordionItem>
+        <h2>
+          <AccordionButton>
+            <Box as="span" flex="1" textAlign="left" fontFamily="Poppins-Medium" fontSize="15px">
+              Servicios y Tarifas
+            </Box>
+            <AccordionIcon />
+          </AccordionButton>
+        </h2>
+        <AccordionPanel pb={4}>
+        <TabServicesAndRates
+            catAccepted={catAccepted}
+            dogAccepted={dogAccepted}
+          />
+        </AccordionPanel>
+      </AccordionItem>
+
+      <AccordionItem>
+        <h2>
+          <AccordionButton>
+            <Box as="span" flex="1" textAlign="left" fontFamily="Poppins-Medium" fontSize="15px">
+              Disponibilidad
+            </Box>
+            <AccordionIcon />
+          </AccordionButton>
+        </h2>
+        <AccordionPanel pb={4}>
+          <TabAvailability />
+        </AccordionPanel>
+      </AccordionItem>
+
+      <AccordionItem>
+        <h2>
+          <AccordionButton>
+            <Box as="span" flex="1" textAlign="left" fontFamily="Poppins-Medium" fontSize="15px">
+              Ubicación
+            </Box>
+            <AccordionIcon />
+          </AccordionButton>
+        </h2>
+        <AccordionPanel pb={4}>
+          <TabLocation />
+        </AccordionPanel>
+      </AccordionItem>
+
+      <AccordionItem>
+        <h2>
+          <AccordionButton>
+            <Box as="span" flex="1" textAlign="left" fontFamily="Poppins-Medium" fontSize="15px">
+              Valoración
+            </Box>
+            <AccordionIcon />
+          </AccordionButton>
+        </h2>
+        <AccordionPanel pb={4}>
+          <TabReview />
+        </AccordionPanel>
+      </AccordionItem>
+    </Accordion>
+  );
+}
