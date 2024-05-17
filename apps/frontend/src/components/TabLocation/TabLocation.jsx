a15707c4d60f3cb6d680328f963d0921468c0e33
FlorenciaNieva <florencianieva930@gmail.com>
2024-05-17 16:31:45 -0300

    Agrego mapa con latitud y longitud desde la base de datos

--- apps/frontend/src/components/TabLocation/TabLocation.jsx
+++ apps/frontend/src/components/TabLocation/TabLocation.jsx
@@ -1,11 +1,23 @@
-import React from "react";
-import { Image, Flex, Heading, Text } from "@chakra-ui/react";
+import React, { useEffect } from "react";
+import { Image, Flex, Heading, Text, Box } from "@chakra-ui/react";
 import IconLocation from "../../assets/icons/IconLocation.svg";
-import Mapa from "../../assets/img/Mapa.png";
+import { MapContainer, TileLayer, Marker, useMap } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
 
-export default function TabLocation() {
+export default function TabLocation({
+  approximateLatitude,
+  approximateLongitude,
+}) {
+  function ResizeMap() {
+    const map = useMap();
+    useEffect(() => {
+      map.invalidateSize();
+    }, [map]);
+    return null;
+  }
+
   return (
-    <>
+    <Box mb="40px">
       <Heading
         fontFamily="Poppins-SemiBold"
         fontSize="24px"
@@ -14,13 +26,28 @@ export default function TabLocation() {
       >
         Ubicación
       </Heading>
-      <Flex gap={2}>
+      <Flex gap={2} mb="40px">
         <Image src={IconLocation} alt="Icono de ubicación" />
         <Text fontFamily="Poppins-Regular" fontSize="14px" color="gris.600">
           Palermo, Buenos Aires
         </Text>
       </Flex>
-      <Image src={Mapa} alt="Imagen de mapa" mt="32px" mb="40px" />
-    </>
+      <Box width="100%" height="500px" borderRadius={20}>
+        <MapContainer
+          center={[approximateLatitude, approximateLongitude]}
+          zoom={8}
+          style={{ height: "100%", width: "100%", zIndex: "-1" }}
+        >
+          <TileLayer
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          />
+          <Marker
+            position={[approximateLatitude, approximateLongitude]}
+          ></Marker>
+          <ResizeMap />
+        </MapContainer>
+      </Box>
+    </Box>
   );
 }
