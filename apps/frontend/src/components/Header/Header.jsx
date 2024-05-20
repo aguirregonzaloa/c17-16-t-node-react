60ec69ae0f756537896a6615d6506b12e657f143
FlorenciaNieva <florencianieva930@gmail.com>
2024-05-20 15:07:02 -0300

    Agrego navegación al logo del header

--- apps/frontend/src/components/Header/Header.jsx
+++ apps/frontend/src/components/Header/Header.jsx
@@ -7,15 +7,20 @@ import {
   Spacer,
   useBoolean,
   useMediaQuery,
+  Link as ChakraLink,
 } from "@chakra-ui/react";
 import LogoBanner from "../../assets/logos/LogoBanner.svg";
 import { IoMenu, IoClose } from "react-icons/io5";
 import NavBar from "../NavBar/NavBar";
+import {
+  useNavigate,
+} from "react-router-dom";
 
 export default function Header() {
   const [scrolled, setScrolled] = useState(false);
   const [isOpen, setIsOpen] = useBoolean();
   const [isSmallerThan640] = useMediaQuery("(max-width: 640px)");
+  const navigate = useNavigate();
 
   useEffect(() => {
     //Modifica el color del header al hacer scroll
@@ -32,17 +37,40 @@ export default function Header() {
     };
   }, []);
 
+  const handleNavClick = (sectionId) => {
+    navigate("/");
+    setTimeout(() => {
+      const sectionElement = document.getElementById(sectionId);
+      if (sectionElement) {
+        window.scrollTo({
+          top: sectionElement.offsetTop,
+          behavior: "smooth",
+        });
+      }
+    }, 100);
+  };
+
   return (
     <header
       style={{
         position: "fixed",
         zIndex: "2",
         width: "100%",
-        backgroundColor: scrolled ? "#FEFBF6" : "",
+        backgroundColor: scrolled ? "#FAFAFA" : "",
       }}
     >
       <Flex align="center" mx={{ base: "20px", md: "100px" }}>
-        <Image boxSize={20} src={LogoBanner} alt="Logo" />
+        <ChakraLink
+          onClick={() => handleNavClick("banner")}
+          alignSelf="center"
+          color="gris.700"
+          fontFamily="Poppins-Medium"
+          _hover={{ color: "azulacento.500" }}
+          _focus={{ color: "azulacento.500" }}
+          _active={{ color: "azulacento.500" }}
+        >
+          <Image boxSize={20} src={LogoBanner} alt="Logo" />
+        </ChakraLink>
         <Spacer />
         {isSmallerThan640 ? (
           <Box>
